71b3c596aba224acb73b50d9035457e185df2151
Christopher Paul Haar <christopherpaul.haar@dkb.de>
2023-06-23 09:47:58 +0200

    feat(id): add connection-secret cluster_id
    Signed-off-by: Christopher Paul Haar <christopherpaul.haar@dkb.de>

--- config/ekscluster/config.go
+++ config/ekscluster/config.go
@@ -9,5 +9,13 @@ func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("castai_eks_cluster", func(r *config.Resource) {
 		r.ShortGroup = ""
 		r.Kind = "EksCluster"
+
+		r.Sensitive.AdditionalConnectionDetailsFn = func(attr map[string]any) (map[string][]byte, error) {
+			conn := map[string][]byte{}
+			if a, ok := attr["id"].(string); ok {
+				conn["attribute.cluster_id"] = []byte(a)
+			}
+			return conn, nil
+		}
 	})
 }
